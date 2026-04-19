35db1c62f401aeb0f4771781329261353fd8a1ad
Leyre Rodriguez Leon <20leyre.rod02@gmail.com>
2026-04-19 21:18:13 +0200

    Añadir parametro cantidad al metodo operativa_cuenta.

--- src/cuentas/Main.java
+++ src/cuentas/Main.java
@@ -5,16 +5,16 @@ public class Main {
     public static void main(String[] args) {
         CCuenta cuenta1;
         cuenta1 = new CCuenta("Antonio López", "1000-2365-85-1230456789", 2500, 0);
-        operativa_cuenta(cuenta1);
+        operativa_cuenta(cuenta1, 2300);
     }
 
-    public static void operativa_cuenta(CCuenta cuenta) {
+    public static void operativa_cuenta(CCuenta cuenta, float cantidad) {
         double saldoActual;
         saldoActual = cuenta.estado();
         System.out.println("El saldo actual es " + saldoActual);
 
         try {
-            cuenta.retirar(2300);
+            cuenta.retirar(cantidad);
         } catch (Exception e) {
             System.out.print("Fallo al retirar");
         }
